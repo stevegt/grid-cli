e189388286ce9825edd6bad0ce1714cec3f58264
Steve Traugott <stevegt@t7a.org>
2024-09-06 08:30:50 -0700

    Maintain output focus when merging 410-zkp.md into 000-TODO.md
    Maintain output focus; merge 410-zkp.md into 000-TODO.md
    
    summary of diff --git  a/.aidda/prompt b/.aidda/prompt
    
    - Add instruction to maintain focus of the output document
    - Update input documents to v2/doc/410-zkp.md and v2/doc/000-TODO.md
    - Update output document to v2/doc/000-TODO.md
    - Change merge instruction to combine 410-zkp.md into 000-TODO.md without changing the focus of 000-TODO.md
    
    Add and modify instructions to maintain document focus
    
    summary of diff --git  a/v2/doc/merge.sh b/v2/doc/merge.sh
    
    - Add instruction to maintain focus of the output document
    - Modify merge instruction to specify maintaining focus of the output document

--- v2/doc/merge.sh
+++ v2/doc/merge.sh
@@ -8,13 +8,14 @@ output=$(ls *.md | randline)
 echo "Mime-Version: 1.0
 Content-Transfer-Encoding: quoted-printable
 Sysmsg: You are an expert technical writer and software architect. 
-    Merge the input documents into the output document.
+    Merge the input documents into the output document.  Maintain the
+    focus of the output document.  
 In: 
     v2/doc/$input
     v2/doc/$output
 Out: v2/doc/$output
 
-Merge $input into $output.
+Merge $input into $output without changing the focus of $output.
 " > ../../.aidda/prompt
 
 grok aidda commit prompt

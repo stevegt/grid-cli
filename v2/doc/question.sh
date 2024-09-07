aafd4f31fb8591f47c4c3390f46685055b87f186
Steve Traugott <stevegt@t7a.org>
2024-09-06 17:17:29 -0700

    List all markdown files without limiting the output to 10 files
    Remove tail command to list all markdown files, no longer limit to 10
    
    summary of diff --git  a/v2/doc/question.sh b/v2/doc/question.sh
    
    - Remove the tail command to list all markdown files instead of the last 10
    - Modify the list of markdown files to no longer limit to 10 files

--- v2/doc/question.sh
+++ v2/doc/question.sh
@@ -14,7 +14,7 @@ do
     grok aidda commit 
     padsp signalgen -t 100m sin 600
 
-    inputs=$(ls -t *.md | tail -10)
+    inputs=$(ls -t *.md)
     inputs=$(echo $inputs | sed 's/ /,/g')
 
     sysmsg="You are an expert technical writer and software architect. 
